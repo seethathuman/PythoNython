1b9323e1c7bacefa0fe1db8384680fbf7f3aebc0
SeeThatHuman <seethathuman@gmail.com>
2025-10-03 16:11:21 +0800

    init commit

--- pythoNython.py
+++ pythoNython.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
-import sys
+from sys import argv
 
 def main(mainfile):
     pass
 
 if __name__ == "__main__":
-    if sys.argv:
-        with open(sys.argv[0]) as f:
+    if len(argv) > 1:
+        with open(argv[1]) as f:
             mainfile = f.readlines()
         main(mainfile)
     else:
-        print("PythoNython 1.0")
+        print("PythoNython test 1\n"
+              "Usage: PythoNython.py file.py")
